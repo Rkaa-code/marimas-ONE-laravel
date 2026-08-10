64d82ff95e72349a7bac90964c603fc12fd1405d
rkaa <mp8359620@gmail.com>
2026-08-10 11:19:57 +0700

    -

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Marimas One</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])A
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased">
     <div class="flex min-h-screen items-center justify-center bg-zinc-100 px-4">
